7c237309349e95be0f572a5f04f5ea9843e17fea
talesmateus123 <talesmateus1999@hotmail.com>
2020-06-08 09:52:01 -0400

    Changes in the new computer page form.

--- src/app/pages/equipments/new/new-equipment.page.ts
+++ src/app/pages/equipments/new/new-equipment.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ComputerDTO, MonitorDTO, EquipmentControllerService } from '../shared';
-import { ProcessorDTO } from '../../electronic-components/shared';
+import { ProcessorDTO, ElectronicComponentControllerService } from '../../electronic-components/shared';
 import { SectorControllerService } from '../../sectors';
 
 @Component({
@@ -13,6 +13,8 @@ export class NewEquipmentPage implements OnInit {
   public equipmentType = "COMPUTER";
 
   public detailForm: boolean = false;
+  public ramMemoryQuantity: number = 0;
+  public storageDeviceQuantity: number = 0;
   
   public formManufacturer: string;
   public formModel: string;
@@ -40,6 +42,81 @@ export class NewEquipmentPage implements OnInit {
   public formStorageDevicesId: number[];
   public formComputerUsersId: number[];
 
+  // Processor attributes
+  public processor_manufacturer: string;
+	public processor_model: string;
+	public processor_description: string;
+	public processor_itWorks: boolean;
+	public processor_name: string;
+	public processor_architecture: string;
+
+  // Ram memories attributes
+  public ramMemory1_manufacturer: string;
+	public ramMemory1_model: string;
+	public ramMemory1_description: string;
+	public ramMemory1_itWorks: boolean;
+	public ramMemory1_sizeInGB: number;
+	public ramMemory1_architecture: string;
+	public ramMemory1_type: string;
+
+  public ramMemory2_manufacturer: string;
+	public ramMemory2_model: string;
+	public ramMemory2_description: string;
+	public ramMemory2_itWorks: boolean;
+	public ramMemory2_sizeInGB: number;
+	public ramMemory2_architecture: string;
+	public ramMemory2_type: string;
+
+  public ramMemory3_manufacturer: string;
+	public ramMemory3_model: string;
+	public ramMemory3_description: string;
+	public ramMemory3_itWorks: boolean;
+	public ramMemory3_sizeInGB: number;
+	public ramMemory3_architecture: string;
+	public ramMemory3_type: string;
+	public ramMemory3_computerId: number;
+
+  public ramMemory4_manufacturer: string;
+	public ramMemory4_model: string;
+	public ramMemory4_description: string;
+	public ramMemory4_itWorks: boolean;
+	public ramMemory4_sizeInGB: number;
+	public ramMemory4_architecture: string;
+	public ramMemory4_type: string;
+
+  // Storage devices attributes
+  public storageDevice1_manufacturer: string;
+	public storageDevice1_model: string;
+	public storageDevice1_description: string;
+	public storageDevice1_itWorks: boolean;
+	public storageDevice1_sizeInGB: number;
+	public storageDevice1_architecture: string;
+	public storageDevice1_type: string;
+  
+  public storageDevice2_manufacturer: string;
+	public storageDevice2_model: string;
+	public storageDevice2_description: string;
+	public storageDevice2_itWorks: boolean;
+	public storageDevice2_sizeInGB: number;
+	public storageDevice2_architecture: string;
+	public storageDevice2_type: string;
+  
+  public storageDevice3_manufacturer: string;
+	public storageDevice3_model: string;
+	public storageDevice3_description: string;
+	public storageDevice3_itWorks: boolean;
+	public storageDevice3_sizeInGB: number;
+	public storageDevice3_architecture: string;
+	public storageDevice3_type: string;
+  
+  public storageDevice4_manufacturer: string;
+	public storageDevice4_model: string;
+	public storageDevice4_description: string;
+	public storageDevice4_itWorks: boolean;
+	public storageDevice4_sizeInGB: number;
+	public storageDevice4_architecture: string;
+	public storageDevice4_type: string;
+
   public availableComputers: ComputerDTO[];
   public availableMonitors: MonitorDTO[];
   public availableProcessors: ProcessorDTO[];
@@ -47,12 +124,33 @@ export class NewEquipmentPage implements OnInit {
   constructor(
     public controller: EquipmentControllerService,
     public sectorController: SectorControllerService,
+    public electronicComponentControllerService: ElectronicComponentControllerService
   ) { }
 
   ngOnInit() {
     this.sectorController.updateSectorsList();
     this.populateAvailableAvailableComputers();
     this.populateAvailableAvailableMonitors();
+  }
+
+  public addRamMemory() {
+    if(this.ramMemoryQuantity < 4)
+    this.ramMemoryQuantity++;
+  }
+
+  public addStorageDevice() {
+    if(this.storageDeviceQuantity < 4)
+      this.storageDeviceQuantity++;
+  }
+
+  public deleteRamMemory() {
+    if(this.ramMemoryQuantity > 0)
+    this.ramMemoryQuantity--;
+  }
+
+  public deleteStorageDevice() {
+    if(this.storageDeviceQuantity > 0)
+      this.storageDeviceQuantity--;
   }
 
   populateAvailableAvailableComputers() {
